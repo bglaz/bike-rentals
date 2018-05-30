fbd748d066c573e12b42a29e18e6a7ccca2318f5
Brian <brian.glaz@gmail.com>
2018-05-30 16:51:48 -0400

    add return to home link

--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -1,66 +1,71 @@
 <template>
-    <section class="product-container">
-        <div class="product-meta">
-            <h2>{{ item.name }}</h2>
-            <img :src="item.image" :alt="item.name" />
-            <div class="price">${{ item.price.toFixed(2) }}</div>
-        </div>
-        <div class="product-desc">
-            <h2>{{ item.name }} Details</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi itaque cumque reprehenderit molestias error officiis repellendus eveniet deserunt eaque laboriosam. Nihil sint at mollitia nobis voluptas earum ea aut. Quo.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quam, ab omnis molestias vitae nulla eos voluptatem eligendi esse est! Distinctio voluptas molestiae nostrum amet magnam! Exercitationem harum alias praesentium.</p>
-            <a class="button" v-if="! reserveClicked" @click.prevent="handleReserveClick">Reserve Now</a>
-
-            <div v-if="reserveClicked">
-                <!-- TODO: refactor into component -->
-                <table>
-                    <thead>
-                        <tr>
-                            <th colspan="3">Add Accessories ?</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                         <tr v-for="accessory in accessories" :key="accessory.id">
-                            <td>
-                                <label :for="`acc_${accessory.name}`">{{ accessory.name }}</label>
-                            </td>
-                            <td><span class="price">${{ accessory.price.toFixed(2) }}</span></td>
-                            <td>
-                                <input type="checkbox" :value="accessory.id" v-model="selectedAccessoryIds" :id="`acc_${accessory.name}`">
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-
-                <!-- TODO: refactor into component -->
-                <table>
-                    <thead>
-                        <tr>
-                            <th colspan="2">Additional Add-ons</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                         <tr v-for="addon in addons" :key="addon.id">
-                            <td>
-                                <label :for="`acc_${addon.name}`">{{ addon.name }}</label>
-                            </td>
-                             <td><span class="price">${{ addon.price.toFixed(2) }}</span></td>
-                            <td>
-                                <input type="checkbox" :value="addon.id" v-model="selectedAddonIds" :id="`acc_${addon.name}`">
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-
-                <div>
-                    <b>Total Price: </b> <span class="price">${{  totalPrice.toFixed(2) }}</span>
-                </div>
-
-                 <a @click.prevent="handleConfirmationClick" class="button">Confirm Reservation</a>
-
+    <div>
+        <section>
+            <nuxt-link to="/">&lt; Return Home</nuxt-link>
+        </section>
+        <section class="product-container">
+            <div class="product-meta">
+                <h2>{{ item.name }}</h2>
+                <img :src="item.image" :alt="item.name" />
+                <div class="price">${{ item.price.toFixed(2) }}</div>
             </div>
-        </div>
-    </section>
+            <div class="product-desc">
+                <h2>{{ item.name }} Details</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi itaque cumque reprehenderit molestias error officiis repellendus eveniet deserunt eaque laboriosam. Nihil sint at mollitia nobis voluptas earum ea aut. Quo.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quam, ab omnis molestias vitae nulla eos voluptatem eligendi esse est! Distinctio voluptas molestiae nostrum amet magnam! Exercitationem harum alias praesentium.</p>
+                <a class="button" v-if="! reserveClicked" @click.prevent="handleReserveClick">Reserve Now</a>
+
+                <div v-if="reserveClicked">
+                    <!-- TODO: refactor into component -->
+                    <table>
+                        <thead>
+                            <tr>
+                                <th colspan="3">Add Accessories ?</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="accessory in accessories" :key="accessory.id">
+                                <td>
+                                    <label :for="`acc_${accessory.name}`">{{ accessory.name }}</label>
+                                </td>
+                                <td><span class="price">${{ accessory.price.toFixed(2) }}</span></td>
+                                <td>
+                                    <input type="checkbox" :value="accessory.id" v-model="selectedAccessoryIds" :id="`acc_${accessory.name}`">
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <!-- TODO: refactor into component -->
+                    <table>
+                        <thead>
+                            <tr>
+                                <th colspan="2">Additional Add-ons</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="addon in addons" :key="addon.id">
+                                <td>
+                                    <label :for="`acc_${addon.name}`">{{ addon.name }}</label>
+                                </td>
+                                <td><span class="price">${{ addon.price.toFixed(2) }}</span></td>
+                                <td>
+                                    <input type="checkbox" :value="addon.id" v-model="selectedAddonIds" :id="`acc_${addon.name}`">
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <div>
+                        <b>Total Price: </b> <span class="price">${{  totalPrice.toFixed(2) }}</span>
+                    </div>
+
+                    <a @click.prevent="handleConfirmationClick" class="button">Confirm Reservation</a>
+
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
